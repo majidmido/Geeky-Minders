98fd4af6892596a6b19bb49d1201bc514e9d8db0
Majid Mido <majidmido03@gmail.com>
2021-11-29 16:34:56 +0100

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
- module.exports = {
-    plugins: [
-      "postcss-import",
-      "tailwindcss",
-      "autoprefixer"
-    ]
-  };
+module.exports = {
+  purge: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        '2/3': '66.666667%',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+
+};
