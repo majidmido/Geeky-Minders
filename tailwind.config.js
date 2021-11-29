c8cee37cc0e1207d7cbeb904b9550c873b9bd7d1
majidmido <71548069+majidmido@users.noreply.github.com>
2021-11-29 15:11:22 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
- purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+ purge: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
   darkMode: false, // or 'media' or 'class'
   theme:{
     extend: {
